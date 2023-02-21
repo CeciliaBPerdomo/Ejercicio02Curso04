a361ebfad7b5f67e38651ea2bc14e4cf6ad185df
Cecilia <cecilia.perdomo@gmail.com>
2023-02-21 15:07:47 -0300

    Ejercicio 02 - Python

---
+++ Ejercicio 02/hola.py
@@ -0,0 +1,7 @@
+# Modifica la variable del anterior ejercicio en la consola de python
+# y después muestrala por consola para ver la modificación de la variable.
+
+hola = "Hola mundo"
+
+hola = "Hola Mundo en el ejercicio 02"
+print(hola)
